6a8c4c054ba7d69e3275590ebcdab6e713db7a3d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-11-06 13:47:02 +0900

    Change: vite-plugin-electronを使うように

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import electron from "vite-plugin-electron";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    electron({
+      entry: "./src-electron/main.ts",
+    }),
+  ],
+});
