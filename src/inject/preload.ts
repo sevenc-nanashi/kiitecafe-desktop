986ae59d107e1b3a5113c1c5d1c9fe2fca14cb60
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-11-06 19:50:07 +0900

    Add: 自動ログインを追加

--- src/inject/preload.ts
+++ src/inject/preload.ts
@@ -1,6 +1,13 @@
 import { ipcRenderer } from "electron";
 import style from "./style.css";
 
+console.log("Preload: loaded");
+setTimeout(() => {
+  if (location.pathname.includes("intro")) {
+    // document.querySelector(".goto_kiite_login_button")!.click();
+    location.href = "https://kiite.jp/login?mode=cafe";
+  }
+}, 0);
 document.addEventListener("DOMContentLoaded", () => {
   const styleElement = document.createElement("style");
   styleElement.textContent = style.toString();
