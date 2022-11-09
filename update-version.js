d8aef5256d8f2fd40c5c161cece7f0e3f32afbba
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-11-09 20:25:07 +0900

    Add: ビルドのworkflowを追加

---
+++ update-version.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+if (!process.argv[2]) {
+  console.log("No version specified")
+  process.exit(1)
+}
+fs.cpSync("package.json", "package.json.bak")
+
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"))
+
+packageJson.version = process.argv[2]
+
+fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2))
