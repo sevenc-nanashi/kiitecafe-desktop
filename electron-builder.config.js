27256506af888905c04363539ab28fcd7ddc57c2
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-06-18 20:27:31 +0900

    Fix: ビルド周りを修正

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -6,6 +6,7 @@ module.exports = {
   productName: "Kiite Cafe Desktop",
   copyright: "2022 © Nanashi. <@sevenc-nanashi>",
   icon: "build/icon.png",
+  nodeGypRebuild: false,
   directories: {
     output: "dist-electron",
   },
